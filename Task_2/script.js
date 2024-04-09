0587dfd91d4fc698732fa22ec3451f268a73d1af
Shivesh Mukund Tripathi <143014986+shiveshmukund@users.noreply.github.com>
2024-04-09 11:36:10 +0530

    script.js

--- Task_2/script.js
+++ Task_2/script.js
@@ -1,12 +1,10 @@
 let celsius = document.getElementById("celsius");
 let fahrenheit = document.getElementById("fahrenheit");
-function celToFar()
-{
+function celToFar(){
     let output = ( parseFloat(celsius.value) * 9/5 ) + 32;
     fahrenheit.value = parseFloat(output.toFixed(2));
 }
-function farToCel()
-{
+function farToCel(){
     let output = ( parseFloat(fahrenheit.value) - 32) * 5/9;
     celsius.value = parseFloat( output.toFixed(2));
     console.log(output);
