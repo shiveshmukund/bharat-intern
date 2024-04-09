955d08fcb769d664fd73febe4151f76e6021b783
Shivesh Mukund Tripathi <143014986+shiveshmukund@users.noreply.github.com>
2024-04-09 11:47:23 +0530

    script.js

--- Task_3/script.js
+++ Task_3/script.js
@@ -1,25 +1,19 @@
 let accordian = document.getElementsByClassName("FAQ-title");
-for (let i = 0; i < accordian.length; i++)
-{
-  accordian[i].addEventListener("click", function ()
-  {
-    if (this.childNodes[1].classList.contains("fa-plus"))
-    {
+for (let i = 0; i < accordian.length; i++){
+  accordian[i].addEventListener("click", function (){
+    if (this.childNodes[1].classList.contains("fa-plus")){
       this.childNodes[1].classList.remove("fa-plus");
       this.childNodes[1].classList.add("fa-times");
     } 
-    else
-    {
+    else{
       this.childNodes[1].classList.remove("fa-times");
       this.childNodes[1].classList.add("fa-plus");
     }
     let content = this.nextElementSibling;
-    if (content.style.maxHeight)
-    {
+    if (content.style.maxHeight){
       content.style.maxHeight = null;
     }
-    else
-    {
+    else{
       content.style.maxHeight = content.scrollHeight + "px";
     }
   });
